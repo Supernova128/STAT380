1a0d5a7c045ec3951c406c7ac5b12632622211d5
Devansh Agarwal <devanshagarwal20@gmail.com>
2021-02-17 15:33:27 -0500

    Added theoretical distribution

--- Coin Flip regression/project/src/models/Train_model.R
+++ Coin Flip regression/project/src/models/Train_model.R
@@ -13,3 +13,18 @@ test$result <- predict(model,newdata = test, type = "response")
 answers <- test[,.(id,result)]
 
 fwrite(answers,'project/volume/data/processed/Submission3.csv')
+
+rm(model,test,train,answers)
+
+# Theoretical distribution 
+
+tmodel <- as.data.table(list(0:10,seq(1/12,11/12,by = 1/12)))
+
+setnames(tmodel,"V1","total")
+setnames(tmodel,"V2","result")
+
+test <- fread('project/volume/data/interim/test.csv')
+
+tanswers <- merge.data.table(test,tmodel, by = "total")[,.(id,result)][order(id)]
+
+fwrite(tanswers,'project/volume/data/processed/SubmissionT.csv')
